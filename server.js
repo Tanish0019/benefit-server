48e72fe4dedd472b643b97019e56caa18b803f50
Prashant <prash.kumar047@gmail.com>
2017-12-13 22:48:39 +0530

    added starter code

--- server.js
+++ server.js
@@ -6,17 +6,21 @@ const index = require('./routes/index'); // The index router
 
 const app = express(); // Define our app using Express
 
+
 // Setting our port --------------
 let port = process.env.PORT || 8080;
 app.set('port',port);
+
 
 app.use(logger('dev')); // Logging the request info on the console
 // Configure server to use bodyParser()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
 // REGISTER OUR ROUTES -------------------------------
 app.use('/', index);
+
 
 // START THE SERVER ----------------------------------
 app.listen(app.get('port'), (error) => {
