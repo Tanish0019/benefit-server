bccc3416899cd6522b1a49f5700e2f920dd7e211
Dhruv Ramdev <pythonbrilliant@gmail.com>
2018-05-27 19:52:41 +0530

    Text Corrections

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -32,11 +32,11 @@ let authController = {
           throw err;
         }
         if(!user) {
-          res.staus(401).send({ message: constants.USER_NOT_EXITS });
+          res.status(401).send({ message: constants.USER_NOT_EXITS });
         } else {
           let validPassword = user.comparePassword(req.body.password);
           if(!validPassword) {
-            res.staus(402).send({ message: constants.INVALID_PASS });
+            res.status(402).send({ message: constants.INVALID_PASS });
           } else {
             let token = new Token(user).getToken();
             res.json({
