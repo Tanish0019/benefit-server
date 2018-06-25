b7cc035735bab8c7902b8150b002b26a3af1598c
Tanish Grover <tanigrover@gmail.com>
2018-06-26 00:16:59 +0530

    Added tracking routes

---
+++ controllers/trackingController.js
@@ -0,0 +1,39 @@
+import Client from '../models/client';
+import Tracking from '../models/Tracking'
+
+const trackingController = {
+	getDetails: (req, res, next) => {
+		Tracking.find({
+			client: req.decoded.id
+		}).then(data => {
+			res.json({
+				success: true,
+				message: "Tracking details found",
+				data: data
+			})
+		}).catch(err => {
+			return next(new Error("Some Error Occured"));
+		})
+	},
+
+	postDetails: (req, res, next) => {
+        Tracking.findOneAndUpdate({
+            client: req.decoded.id ,
+            date: req.body.date,
+            time: req.body.time
+        }, req.body, {
+        	upsert: true,
+        	new: true
+        }).then((data) => {
+        	res.json({
+                success: true,
+                message: "Tracking details Updated",
+                data: data
+            });
+        }).catch(err => {
+            return next(new Error("Some Error Occured"));
+        })
+    }
+}
+
+export default trackingController;
